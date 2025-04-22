3f7c735dce8db28fbcc79c6e3171131debe87ea1
draganovdimitar2 <draganovdimitar2@gmail.com>
2025-04-22 12:00:00 +0300

    feat: implmeneted black pawns moving logic

--- chess/main.py
+++ chess/main.py
@@ -1,7 +1,7 @@
 import pygame
+from sys import exit
 from chess.pieces.black.black_pawn import BlackPawn
-
-SQUARE_SIZE = 100
+from settings import SQUARE_SIZE, SCREEN_SIZE
 
 
 def draw_board():
@@ -15,17 +15,44 @@ def draw_board():
 
 running = True
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
-black_pawn = BlackPawn(200, 200)
-pawn_group = pygame.sprite.GroupSingle()
-pawn_group.add(black_pawn)
+screen = pygame.display.set_mode(SCREEN_SIZE)
+clock = pygame.time.Clock()
+
+black_pieces_group = pygame.sprite.Group()  # the group for black pieces
+for i in range(8):  # spawn all 8 pawns
+    black_pawn = BlackPawn((SQUARE_SIZE * i), SQUARE_SIZE)
+    black_pieces_group.add(black_pawn)
 
 while running:
     draw_board()
+
+    black_pieces_group.draw(screen)
     pygame.display.update()
-    pawn_group.draw(screen)
 
     for event in pygame.event.get():  # to get ell the events and loop through them
         if event.type == pygame.QUIT:  # to close the window
             pygame.quit()  # opposite to pygame.init()
             running = False
+            exit()
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouse_pos = pygame.mouse.get_pos()
+            for piece in black_pieces_group:
+                # Only one pawn should be selected at a time
+                if not piece.rect.collidepoint(mouse_pos) and piece.selected:  # if the piece is already selected move it
+                    current_possible_moves = piece.current_possible_moves()  # fetch all possible moves
+                    print(current_possible_moves)
+                    for move in current_possible_moves:
+                        move_rect = pygame.Rect(move[0], move[1], SQUARE_SIZE, SQUARE_SIZE)
+                        if move_rect.collidepoint(mouse_pos):
+                            piece.move(move)
+                            print(piece.selected)
+                elif piece.rect.collidepoint(mouse_pos) and not piece.selected:  # first select the piece
+                    piece.selected = True
+                    if piece.selected:
+                        print(f"x = {piece.rect.x}, y = {piece.rect.y}")
+                        print(mouse_pos)
+                        print(piece.selected)
+
+
+    clock.tick(60)
--- chess/pieces/black/black_pawn.py
+++ chess/pieces/black/black_pawn.py
@@ -1,20 +1,32 @@
 import pygame
+from chess.settings import SQUARE_SIZE, X_UPPER_BOUNDARY, X_LOWER_BOUNDARY, Y_UPPER_BOUNDARY, Y_LOWER_BOUNDARY
 
 
 class BlackPawn(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.x = x
-        self.y = y
-        self.image = pygame.image.load('images/black_pieces/pawn_b.png')
-        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(
+            pygame.image.load('images/black_pieces/pawn_b.png').convert_alpha(),
+            (SQUARE_SIZE, SQUARE_SIZE)
+        )  # make the image 100x100 pixels
+        self.rect = self.image.get_rect(topleft=(x, y))  # by default it is positioned on (0, 0)
         self.selected = False
+        self.original_y = y  # to track first move (to know if the pawn can go 2 squares forward)
 
-    def player_input(self, screen, square_size):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_DOWN]:
-            self.selected = True
-            selected_color = (204, 255, 204)
+    def current_possible_moves(self):
+        moves = []
+        # One step forward
+        one_step = (self.rect.x, self.rect.y + SQUARE_SIZE)
+        if one_step[1] <= Y_UPPER_BOUNDARY:
+            moves.append(one_step)
+        # Two steps forward (only from starting row)
+        if self.rect.y == self.original_y:
+            two_steps = (self.rect.x, self.rect.y + 2 * SQUARE_SIZE)
+            if two_steps[1] <= Y_UPPER_BOUNDARY:
+                moves.append(two_steps)
+        return moves
 
-            pygame.draw.rect(screen, selected_color,
-                             pygame.Rect(self.x - square_size, self.y + square_size, square_size, square_size))
+    def move(self, positions):
+        self.rect.x = positions[0]
+        self.rect.y = positions[1]
+        self.selected = False
